9c265f32db1d38a122765f63229fc6fbe4b15698
jywkim <jkim589@gmail.com>
2018-09-28 18:25:07 -0400

    initial commit
    dotnet new console -o csharp-usingattributes

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_usingattributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
